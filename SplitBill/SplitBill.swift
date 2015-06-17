8f1a125a416283bc0ec7589316250077d19b922d
Xinyi Yin <zephyrYin@yinxins-mbp.attlocal.net>
2015-06-17 00:37:06 -0700

    sort

--- SplitBill/SplitBill.swift
+++ SplitBill/SplitBill.swift
@@ -8,6 +8,29 @@
 
 import Foundation
 
+struct Option{
+    var originalPrices:[Float]
+    var payMoney:[Int]
+    
+    var shouldPay:Float{
+        get{
+            return originalPrices.reduce(0, combine: +)
+        }
+    }
+    
+    var totalPay:Int{
+        get{
+            return payMoney.reduce(0, combine: +)
+        }
+    }
+    
+    var overFlow:Float{
+        get{
+            return Float(totalPay) - shouldPay
+        }
+    }
+}
+
 
 class SplitBill:NSObject{
     var orignialPrices:[Float]
@@ -27,28 +50,30 @@ class SplitBill:NSObject{
     }
     
     func PrintOriginalPrice(){
-        println("original prices:")
-        for price in self.orignialPrices{
-            println(price)
-        }
+        println("original prices: \(self.orignialPrices)")
     }
     
     func Split(){
         var shouldPay:[Float] = self.orignialPrices.map{return $0*(1+self.taxRate*2)}
         println(shouldPay)
-        var options:Array<[Int]> = shouldPay.map{return [Int(floor($0)), Int(ceil($0))]}
-        var combinations:Array<[Int]> = self.GetCombinations(options)
-        for c in combinations{
-            println(c)
-            println(c.reduce(0, combine: +))
+        var total:Float = shouldPay.reduce(0, combine: +)
+        println("whole price : \(total)")
+        
+        var options:[Option] = self.GetCombinations(shouldPay)
+        options.sort({abs($0.overFlow) < abs($1.overFlow)})
+        for option in options{
+            print(option.originalPrices)
+            print(option.payMoney)
+            println("\(option.totalPay) \(option.overFlow)")
         }
     }
     
-    func GetCombinations(options:Array<[Int]>) -> Array<[Int]>{
+    func GetCombinations(shouldPay:[Float]) -> [Option]{
+        var modifiedPrices:Array<[Int]> = shouldPay.map{return [Int(floor($0)), Int(ceil($0))]}
         var combinations:Array<[Int]> = Array<[Int]>()
-        for option in options{
+        for prices in modifiedPrices{
             if combinations.count==0{
-                for value in option{
+                for value in prices{
                     var path:[Int] = [Int]()
                     path.append(value)
                     combinations.append(path)
@@ -56,7 +81,7 @@ class SplitBill:NSObject{
             }else{
                 var cnt = combinations.count
                 for c in combinations{
-                    for value in option{
+                    for value in prices{
                         var tmp:[Int] = c
                         tmp.append(value)
                         combinations.append(tmp)
@@ -65,6 +90,10 @@ class SplitBill:NSObject{
                 combinations.removeRange(0..<cnt)
             }
         }
-        return combinations
+        var options:[Option] = [Option]()
+        for c in combinations{
+            options.append(Option(originalPrices: shouldPay, payMoney: c))
+        }
+        return options
     }
 }
