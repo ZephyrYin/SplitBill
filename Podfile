80e5782d6088a8c3a53dc3b5f7850c0685e6aabd
Xinyi Yin <zephyrYin@yinxins-mbp.attlocal.net>
2015-06-25 00:01:14 -0700

    scan receipt success

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+
+target 'SplitBill' do
+pod 'TesseractOCRiOS', '4.0.0'
+end
+
+target 'SplitBillTests' do
+
+end
+
--- SplitBill/ViewController.swift
+++ SplitBill/ViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, G8TesseractDelegate {
     var sB:SplitBill = SplitBill()
     override func viewDidLoad() {
         super.viewDidLoad()
+        var tesseract:G8Tesseract = G8Tesseract(language:"eng");
+        tesseract.delegate = self;
+        //tesseract.charWhitelist = "01234567890";
+        tesseract.image = UIImage(named: "Images/receipt.jpeg");
+        tesseract.recognize();
+        
+        NSLog("%@", tesseract.recognizedText);
+        
         var test:[Float] = [3.4, 5.6, 10.1]
         sB.SetOriginalPrice(test)
         sB.SetTaxRate(0.0875)
